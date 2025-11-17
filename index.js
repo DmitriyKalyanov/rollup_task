4d6413bbd1284b478a900dc564049e1f951bdc51
Dmitriy Kalyanov <lightbearer1337@gmail.com>
2025-11-17 20:56:15 +0500

    create title and image

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+import image from './assets/jsLogo.png';
+
+const titleHTML = document.createElement('h1');
+titleHTML.textContent = 'I Love JavaScript';
+
+const imgHTML = document.createElement('img');
+imgHTML.className = 'img';
+imgHTML.src = image;
+
+document.body.append(titleHTML, imgHTML);
+
 console.log('Hello World!');
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,20 @@
+import styles from 'rollup-plugin-styles';
+import image from '@rollup/plugin-image';
+import { babel } from '@rollup/plugin-babel';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+
 export default {
   input: './index.js',
   output: {
     file: './dist/bundle.js',
     format: 'cjs',
   },
-//   plugins: [resolve()],
+  plugins: [
+    styles(),
+    image(),
+    serve(),
+    babel({ babelHelpers: 'bundled' }),
+    livereload(),
+  ],
 };
